d49318d62e4943e10c49893985e433b8e0850e4a
ThiliniYA <thiliniyapa88@gmail.com>
2025-10-22 13:57:03 +1300

    Add DashboardSteps.cs

---
+++ Steps/DashboardSteps.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using Reqnroll;
+using NUnit.Framework;
+using qa_dotnet_cucumber.Pages;
+
+namespace qa_dotnet_cucumber.Steps
+{
+    [Binding]
+    public class DashboardSteps
+    {
+        private readonly DashboardPage _dashboardPage;
+
+        public DashboardSteps(DashboardPage dashboardPage)
+        {
+            _dashboardPage = dashboardPage;
+        }
+
+        [Then("I should see the welcome message")]
+        public void ThenIShouldSeeTheWelcomeMessage()
+        {
+            Assert.That(_dashboardPage.GetWelcomeMessage(), Does.Contain("Welcome"),
+                "User should see a welcome message after logging in.");
+        }
+    }
+}
